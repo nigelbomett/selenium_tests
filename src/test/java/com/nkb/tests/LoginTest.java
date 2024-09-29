adc1b20037fb53f36dfdde93a6a9de8c473a5f39
Nigel Bomett <nigelbomett@gmail.com>
2024-09-29 22:38:22 +0300

    setup tests for login page

--- src/test/java/com/nkb/tests/LoginTest.java
+++ src/test/java/com/nkb/tests/LoginTest.java
@@ -3,14 +3,19 @@ package com.nkb.tests;
 import com.nkb.pages.LoginPage;
 import com.nkb.pages.SecureAreaPage;
 import com.nkb.utils.BaseTest;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
 import jdk.jfr.Description;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-
+@Epic("Login Tests Epic")
+@Feature("Valid Login Features")
 public class LoginTest extends BaseTest {
 
     @Test
+    @Story("User tries to login the system with valid username and valid password")
     @Description("Test to verify login functionality")
     public void loginTest(){
         driver.get(config.baseUrl() + "/login");
