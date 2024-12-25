811b422cfa77d13c0199e98476484c333c164cfb
Nigel Bomett <nigelbomett@gmail.com>
2024-12-25 10:07:49 +0300

    add Chrome Options

--- src/test/java/com/nkb/utils/BaseTest.java
+++ src/test/java/com/nkb/utils/BaseTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.TestInstance;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
 
@@ -20,9 +21,14 @@ public class BaseTest {
     public void setup(){
         config = ConfigCache.getOrCreate(ProjectConfig.class);
 
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
         if("chrome".equalsIgnoreCase(config.browser())){
             WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
+            driver = new ChromeDriver(options);
         }
 
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
